fed0eec2f927f0480167e9bc129d042adf8181d8
Arjun Patrawala <arjunpatrawala@berkeley.edu>
2022-02-20 01:49:32 -0800

    add websockets and tests

--- main.go
+++ main.go
@@ -1,4 +1,38 @@
-package exchange
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func handleWSClient(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, msg, err := c.ReadMessage()
+		if err != nil || mt != 1 {
+			log.Println("Read:", err)
+			break
+		}
+		fmt.Println(mt, string(msg))
+		c.WriteMessage(1, []byte("right back at you"))
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
 
 func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/ws", handleWSClient)
+	http.ListenAndServe(":8080", nil)
 }
--- orderbook.go
+++ orderbook.go
@@ -1,4 +1,4 @@
-package exchange
+package main
 
 import (
 	"container/heap"
